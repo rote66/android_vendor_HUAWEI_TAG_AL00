769299a8bdbfe8e0016c237510a4fcb059bef1d6
rote66 <pontifex23872388@gmail.com>
2019-02-17 13:19:10 +0800

    re-test 2/5?

--- prebuilt/libs/Android.mk
+++ prebuilt/libs/Android.mk
@@ -12,15 +12,15 @@ LOCAL_SRC_FILES_32  := $(PROPRIETARY_PATH)/lib/libcfgdevparam.so
 LOCAL_MULTILIB := both
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE        := libmeizucamera
-LOCAL_MODULE_TAGS   := optional
-LOCAL_MODULE_CLASS  := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-LOCAL_SRC_FILES_64  := $(PROPRIETARY_PATH)/lib64/libmeizucamera.so
-LOCAL_SRC_FILES_32  := $(PROPRIETARY_PATH)/lib/libmeizucamera.so
-LOCAL_MULTILIB := both
-include $(BUILD_PREBUILT)
+#include $(CLEAR_VARS)
+#LOCAL_MODULE        := libmeizucamera
+#LOCAL_MODULE_TAGS   := optional
+#LOCAL_MODULE_CLASS  := SHARED_LIBRARIES
+#LOCAL_MODULE_SUFFIX := .so
+#LOCAL_SRC_FILES_64  := $(PROPRIETARY_PATH)/lib64/libmeizucamera.so
+#LOCAL_SRC_FILES_32  := $(PROPRIETARY_PATH)/lib/libmeizucamera.so
+#LOCAL_MULTILIB := both
+#include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE        := libperfservice
